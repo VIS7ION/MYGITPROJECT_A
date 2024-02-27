e54e883155dbeabc5af3937a202f41465a14c6d1
VIS7ION <fosterawor@gmail.com>
2024-02-27 01:07:17 +0000

    VS CODE FIRST COMMIT

---
+++ gridline.py
@@ -0,0 +1,55 @@
+def gridline():
+    print('+', end=' ')
+    print('- '*4, end='')
+    print('+', '- '*4, '+')
+
+
+    print('+',end='')
+    print(' '*8,'+', end='')
+    print(' '*8,' +')
+    
+    print('+',end='')
+    print(' '*8,'+', end='')
+    print(' '*8,' +')
+   
+    print('+',end='')
+    print(' '*8,'+', end='')
+    print(' '*8,' +')
+  
+  
+    print('+', end=' ')
+    print('- '*4, end='')
+    print('+', '- '*4, '+')
+
+
+    print('+',end='')
+    print(' '*8,'+', end='')
+    print(' '*8,' +')
+   
+    print('+',end='')
+    print(' '*8,'+', end='')
+    print(' '*8,' +')
+   
+    print('+',end='')
+    print(' '*8,'+', end='')
+    print(' '*8,' +')
+   
+   
+    print('+', end=' ')
+    print('- '*4, end='')
+    print('+', '- '*4, '+')
+
+gridline()
+
+
+    
+
+
+
+ 
+
+
+
+
+
+
